c7915c5b1fde60f96b8cf97fbd8fa0076f0973a0
redekeys <renaud.dekeyser@gmail.com>
2022-05-29 17:55:39 +0200

    Try

---
+++ target/generated-sources/annotations/model/Persong_.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-05-29T13:02:22")
+@StaticMetamodel(Persong.class)
+public class Persong_ { 
+
+    public static volatile SingularAttribute<Persong, String> firstname;
+    public static volatile SingularAttribute<Persong, Integer> iDperson;
+    public static volatile SingularAttribute<Persong, String> name;
+    public static volatile SingularAttribute<Persong, Date> dateOfBirth;
+
+}
